77d0c43394aa62aae60cc36f876409b5a5dae58c
RisikaManoharan <108684476+RisikaManoharan@users.noreply.github.com>
2024-09-09 13:50:02 +0530

    Add files via upload

---
+++ todo-list/file.js
@@ -0,0 +1,12 @@
+var ul = document.getElementById("list-container")
+var input =document.getElementById("input")
+function add()
+{
+   var listitem = document.createElement("li")
+   listitem.innerHTML=input.value+"<button onclick=\"deleteItem(event)\">Delete</button>"
+   ul.append(listitem)
+   input.value=" "
+}
+function deleteItem(event){
+  event.target.parentElement.remove()
+}
